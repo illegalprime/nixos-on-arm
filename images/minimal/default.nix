925de54f576f0001e728cab628998b943fb3af4a
Michael Eden <themichaeleden@gmail.com>
2019-03-24 21:10:16 -0400

    set low priorities on options in the minimal image

--- images/minimal/default.nix
+++ images/minimal/default.nix
@@ -1,21 +1,22 @@
 { config, pkgs, lib, ... }:
+with lib;
 {
   imports = [
     ../base
   ];
 
   # try to get rid of unneeded stuff in the image
-  environment.noXlibs = true;
-  documentation.info.enable = false;
-  documentation.man.enable = false;
-  programs.command-not-found.enable = false;
-  networking.firewall.enable = false;
+  environment.noXlibs = mkDefault true;
+  documentation.info.enable = mkDefault false;
+  documentation.man.enable = mkDefault false;
+  programs.command-not-found.enable = mkDefault false;
+  networking.firewall.enable = mkDefault false;
 
   # Automatically log in at the virtual consoles.
-  services.mingetty.autologinUser = "root";
+  services.mingetty.autologinUser = mkDefault "root";
 
   # Allow the user to log in as root without a password.
-  users.users.root.initialHashedPassword = "";
+  users.users.root.initialHashedPassword = mkOverride 999 "";
 
   environment.systemPackages = with pkgs; [
     file
@@ -25,16 +26,19 @@
     htop
   ];
 
-  # setup networking & SSH
-  networking.hostName = "nixos-on-arm";
-  networking.useNetworkd = true;
+  # networking
+  networking.hostName = mkDefault "nixos-on-arm";
   networking.nameservers = [ "1.1.1.1" "8.8.8.8" "8.8.4.4" ];
+
+  # SSH
   services.openssh.enable = true;
-  services.openssh.permitRootLogin = "yes";
+  services.openssh.permitRootLogin = mkDefault "yes";
+
+  # DNS
   services.resolved.enable = true;
   services.resolved.dnssec = "false";
 
   # set a default root password
-  users.mutableUsers = false;
-  users.users.root.password = "toor";
+  users.mutableUsers = mkDefault false;
+  users.users.root.password = mkDefault "toor";
 }
