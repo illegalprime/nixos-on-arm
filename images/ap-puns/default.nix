e57a10dbddc4e1a27e2cb572dd6d80573e96747d
Michael Eden <themichaeleden@gmail.com>
2019-04-26 15:38:58 -0400

    ap-puns: only add the firmware we need to the image to save on space

--- images/ap-puns/default.nix
+++ images/ap-puns/default.nix
@@ -1,10 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 {
   imports = [
     ../mini
     ./service.nix
   ];
 
+  #
+  # NOTE: this is built to work with the ralink 2870 chipset.
+  # https://www.amazon.com/150Mbps-Adapter-LOTEKOO-Wireless-Raspberry/dp/B06Y2HKT75
+  # to use your own WiFi dongle, add its kernel modules and firmware
+  #
+
+  # add the WiFi kernel modules
   boot.kernelPatches = [
     {
       name = "ralink-chipset";
@@ -23,5 +30,13 @@
         RT2X00_LIB_LEDS y
       '';
     }
+  ];
+
+  # add the WiFi firmware
+  hardware.firmware = [
+    (pkgs.runCommand "rt2870" {} ''
+      mkdir -p $out/lib/firmware
+      cp ${pkgs.firmwareLinuxNonfree}/lib/firmware/rt2870.bin $out/lib/firmware/
+    '')
   ];
 }
