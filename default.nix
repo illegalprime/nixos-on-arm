6d0b53926ecd106e8d85a623af5cc709a7153f44
Michael Eden <themichaeleden@gmail.com>
2019-04-27 17:55:51 -0400

    images: factor out sdImage and machine from base image, prep for mender

--- default.nix
+++ default.nix
@@ -1,5 +1,13 @@
 let
-  nixos = import <nixpkgs/nixos> { configuration = <image>; };
+  nixos = import <nixpkgs/nixos> {
+    configuration = { ... }: {
+      imports = [
+        <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
+        <machine>
+        <image>
+      ];
+    };
+  };
 in
 nixos.config.system.build.sdImage // {
   inherit (nixos) pkgs system config;
--- images/base/default.nix
+++ images/base/default.nix
@@ -1,10 +1,5 @@
 { config, pkgs, lib, ... }:
 {
-  imports = [
-    <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
-    <machine>
-  ];
-
   # this value determines the NixOS release with which your system is to be
   # compatible, in order to avoid breaking some software such as database
   # servers. You should change this only after NixOS release notes say you
