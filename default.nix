3e669bdd94c00cfac29c03e43b3667bc9ca553c3
Michael Eden <themichaeleden@gmail.com>
2019-04-28 18:52:14 -0400

    outputs: create new outputs dir for different types of artifact generators

--- default.nix
+++ default.nix
@@ -1,15 +1,1 @@
-let
-  nixos = import <nixpkgs/nixos> {
-    configuration = { ... }: {
-      imports = [
-        ./crosspkgs/modules # extra nixos modules
-        <nixpkgs/nixos/modules/installer/cd-dvd/sd-image.nix>
-        <machine>
-        <image>
-      ];
-    };
-  };
-in
-nixos.config.system.build.sdImage // {
-  inherit (nixos) pkgs system config;
-}
+outputs/sd-image/default.nix
