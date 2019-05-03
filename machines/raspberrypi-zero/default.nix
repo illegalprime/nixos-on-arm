9288a2573cd972ecddd42c8b668a8b0b54d06eaa
Michael Eden <themichaeleden@gmail.com>
2019-05-03 10:30:38 -0400

    machines: raspberrypi-zero update to use new extlinux boot builder

--- machines/raspberrypi-zero/default.nix
+++ machines/raspberrypi-zero/default.nix
@@ -1,25 +1,20 @@
 { pkgs, config, lib, ... }:
-let
-  extlinux = <nixpkgs/nixos/modules/system/boot/loader/generic-extlinux-compatible/extlinux-conf-builder.nix>;
-  extlinux-conf-builder = import extlinux { pkgs = pkgs.buildPackages; };
-in
-with lib;
 {
   imports = [
     ./otg.nix
   ];
 
   # pick the right kernel
-  boot.consoleLogLevel = mkDefault 7;
+  boot.consoleLogLevel = lib.mkDefault 7;
   boot.kernelPackages = pkgs.linuxPackages_5_0;
 
   # set cross compiling
-  nixpkgs.crossSystem = systems.elaborate systems.examples.raspberryPi;
+  nixpkgs.crossSystem = lib.systems.elaborate lib.systems.examples.raspberryPi;
 
   # disable other boot loaders
   boot.loader.grub.enable = false;
   boot.loader.systemd-boot.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = false;
+  boot.loader.generic-extlinux-compatible.enable = true;
 
   # setup the boot loader & config.txt
   sdImage.populateBootCommands = let
@@ -32,17 +27,18 @@ with lib;
       # make debugging easier
       enable_uart=1
 
-      ${optionalString config.boot.otg.enable "dtoverlay=dwc2"}
+      ${lib.optionalString config.boot.otg.enable "dtoverlay=dwc2"}
     '';
-  in ''
+  in with config.system.build; ''
     (
       cd ${pkgs.raspberrypifw}/share/raspberrypi/boot
       cp bootcode.bin fixup*.dat start*.elf $NIX_BUILD_TOP/boot/
     )
+
     cp ${pkgs.ubootRaspberryPiZero}/u-boot.bin boot/kernel.img
+
     cp ${configTxt} boot/config.txt
-    ${extlinux-conf-builder} -t 1 \
-      -c ${config.system.build.toplevel} \
-      -d ./boot
+
+    ${installBootLoaderNative} ${toplevel} -d boot
   '';
 }
