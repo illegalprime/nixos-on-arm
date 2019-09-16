3464e7734e641cf12c3f62608ba2a19c37b760e2
Michael Eden <themichaeleden@gmail.com>
2019-09-16 11:15:22 -0400

    machines: standardize beaglebone to extlinux and clean it up

--- machines/beaglebone/default.nix
+++ machines/beaglebone/default.nix
@@ -1,60 +1,49 @@
 { lib, config, pkgs, ... }:
-let
-  beaglebone = import ./system.nix;
-in
 {
   # specify the system we're compiling to
-  nixpkgs.crossSystem = beaglebone;
+  nixpkgs.crossSystem = lib.systems.elaborate {
+    config = "armv7l-unknown-linux-gnueabihf";
+    platform = {
+      name = "beaglebone";
+      kernelBaseConfig = "multi_v7_defconfig";
+      kernelAutoModules = false;
+      kernelTarget = "zImage";
+      kernelMajor = "2.6";
+      kernelArch = "arm";
+      kernelDTB = true;
+      kernelPreferBuiltin = true;
+      gcc = {
+        cpu = "cortex-a8";
+        fpu = "neon";
+      };
+    };
+  };
 
   # specify a good kernel version
   boot.kernelPackages = pkgs.linuxPackages_5_0;
 
-  # do our own boot-loader
+  # enable the UART connection
+  boot.kernelParams = ["console=ttyO0,115200n8"];
+
+  #
+  # Bootloader (UBoot, extlinux)
+  #
   boot.loader.grub.enable = false;
   boot.loader.systemd-boot.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = false;
-
-  # build & install boot loader
+  boot.loader.generic-extlinux-compatible = {
+    enable = true;
+    dtbs = ["am335x-bonegreen.dtb"];
+  };
   sdImage.populateBootCommands = let
-    kernel = beaglebone.platform.kernelTarget;
-    init = "${config.system.build.toplevel}/init";
-    root = ''/dev/mmcblk''${mmcdev}p2"'';
     uboot = pkgs.buildUBoot {
       defconfig = "am335x_evm_defconfig";
-      extraMeta.platforms = [beaglebone.system];
+      extraMeta.platforms = ["armv7l-linux"];
       filesToInstall = ["MLO" "u-boot.img"];
-      # NOTE: by default u-boot is built to read uEnv.txt from the SD card,
-      # we modify it to attempt to read uEnv.txt from the eMMC as well.
-      extraConfig = lib.strings.replaceStrings ["\n"] [" "] ''CONFIG_BOOTCOMMAND="
-        if test ''${boot_fit} -eq 1; then
-          run update_to_fit;
-        fi;
-        run findfdt;
-        run init_console;
-        run envboot;
-        run bootcmd_mmc1;
-        run bootcmd_legacy_mmc1;
-        run envboot;
-      "'';
     };
-    uEnv = pkgs.writeText "uEnv.txt" ''
-      bootdir=
-      bootfile=${kernel}
-      fdtfile=${beaglebone.dtb}
-      loadaddr=0x80007fc0
-      fdtaddr=0x80F80000
-      loadfdt=fatload mmc ''${mmcdev}:1 ''${fdtaddr} ''${fdtfile}
-      loaduimage=fatload mmc ''${mmcdev}:1 ''${loadaddr} ''${bootfile}
-      uenvcmd=mmc rescan; run loaduimage; run loadfdt; run fdtboot
-      fdtboot=run mmc_args; run mmcargs; bootz ''${loadaddr} - ''${fdtaddr}
-      mmc_args=setenv bootargs console=''${console} ''${optargs} root=${root} rootfstype=ext4 init=${init}
-    '';
-  in ''
+  in with config.system.build; ''
     cp ${uboot}/MLO boot/
     cp ${uboot}/u-boot.img boot/
-    cp ${config.boot.kernelPackages.kernel}/${kernel} boot/
-    cp ${config.boot.kernelPackages.kernel}/dtbs/${beaglebone.dtb} boot/
-    cp ${uEnv} boot/uEnv.txt
+    ${installBootLoaderNative} ${toplevel} -d boot
   '';
 
   #
