77cea413e7c762ac15deac985c2340b15716e243
Michael Eden <themichaeleden@gmail.com>
2019-09-16 18:17:20 -0400

    machines: cleaned up raspberry pi 2, now works with nixops!

--- machines/raspberrypi-2/default.nix
+++ machines/raspberrypi-2/default.nix
@@ -8,15 +8,13 @@
     config = "armv7l-unknown-linux-gnueabihf";
     platform = {
       name = "raspberrypi2";
-      kernelMajor = "2.6"; # Using "2.6" enables 2.6 kernel syscalls in glibc.
+      kernelMajor = "2.6";
       kernelBaseConfig = "multi_v7_defconfig";
       kernelArch = "arm";
       kernelDTB = true;
       kernelAutoModules = true;
       kernelPreferBuiltin = true;
       kernelTarget = "zImage";
-      kernelExtraConfig = ''
-      '';
       gcc = {
         cpu = "cortex-a7";
         fpu = "neon-vfpv4";
@@ -25,23 +23,14 @@
   };
 
   # setup boot loader
-  boot.loader.grub.enable = false;
-  boot.loader.systemd-boot.enable = false;
-  boot.loader.generic-extlinux-compatible.enable = false;
   boot.loader.raspberryPi = {
     enable = true;
     uboot.enable = true;
     version = 2;
   };
-
-  # TODO: extlinux-conf-builder.sh: line 123: cd: /nix/var/nix/profiles: No such file or directory
-  # TODO: contamination:
-  # https://github.com/NixOS/nixpkgs/blob/master/nixos/modules/system/boot/loader/raspberrypi/uboot-builder.nix#L28
-  # TODO: need different boot loader builders for host & build systems
   sdImage.populateBootCommands = with config.system.build; ''
-    ${installBootLoader} ${toplevel} -d ./boot
+    ${installBootLoaderNative} ${toplevel} -d boot
   '';
 
-  # TODO: don't load every dtb possible
-  sdImage.bootSize = 64;
+  sdImage.bootSize = lib.mkOverride 1050 32;
 }
