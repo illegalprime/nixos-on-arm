ee3a6248ce8b056f77d9f7f8aaa295f730419438
Michael Eden <themichaeleden@gmail.com>
2019-09-16 18:18:34 -0400

    machines: added raspberry pi 3, works with nixops!

---
+++ machines/raspberrypi-3/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, config, lib, ... }:
+{
+  # pick the right kernel
+  boot.kernelPackages = pkgs.linuxPackages_5_0;
+
+  # set cross compiling
+  nixpkgs.crossSystem = lib.systems.elaborate {
+    config = "aarch64-unknown-linux-gnu";
+    platform = {
+      name = "raspberrypi3";
+      kernelMajor = "2.6";
+      kernelBaseConfig = "defconfig";
+      kernelArch = "arm64";
+      kernelDTB = true;
+      kernelAutoModules = true;
+      kernelPreferBuiltin = true;
+      kernelTarget = "Image";
+      gcc = {
+        cpu = "cortex-a53+crypto";
+        arch = "armv8-a+crc";
+      };
+    };
+  };
+
+  # setup boot loader
+  boot.loader.raspberryPi = {
+    enable = true;
+    uboot.enable = true;
+    version = 3;
+  };
+  sdImage.populateBootCommands = with config.system.build; ''
+    ${installBootLoaderNative} ${toplevel} -d boot
+  '';
+
+  sdImage.bootSize = lib.mkOverride 1050 64;
+}
