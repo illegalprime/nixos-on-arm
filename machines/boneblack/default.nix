530a761ce7882d480a63d23a1c3e807f99f08d88
Ross MacLeod <ross+github@vitalbio.com>
2020-01-15 11:42:34 -0500

    simple BeagleBone Black support by copying bonegreen and using the boneblack DTB

---
+++ machines/boneblack/default.nix
@@ -0,0 +1,75 @@
+{ lib, config, pkgs, ... }:
+{
+  # specify the system we're compiling to
+  nixpkgs.crossSystem = lib.systems.elaborate {
+    config = "armv7l-unknown-linux-gnueabihf";
+    platform = {
+      name = "beaglebone";
+      kernelBaseConfig = "multi_v7_defconfig";
+      kernelAutoModules = false;
+      kernelTarget = "zImage";
+      kernelMajor = "2.6";
+      kernelArch = "arm";
+      kernelDTB = true;
+      kernelPreferBuiltin = true;
+      gcc = {
+        cpu = "cortex-a8";
+        fpu = "neon";
+      };
+    };
+  };
+
+  # specify a good kernel version
+  boot.kernelPackages = pkgs.linuxPackages_5_0;
+
+  # enable the UART connection
+  boot.kernelParams = ["console=ttyO0,115200n8"];
+
+  #
+  # Bootloader (UBoot, extlinux)
+  #
+  boot.loader.generic-extlinux-compatible = {
+    enable = true;
+    dtbs = ["am335x-boneblack.dtb"];
+  };
+  sdImage.populateBootCommands = let
+    uboot = pkgs.buildUBoot {
+      defconfig = "am335x_evm_defconfig";
+      extraMeta.platforms = ["armv7l-linux"];
+      filesToInstall = ["MLO" "u-boot.img"];
+    };
+  in with config.system.build; ''
+    cp ${uboot}/MLO boot/
+    cp ${uboot}/u-boot.img boot/
+    ${installBootLoaderNative} ${toplevel} -d boot
+  '';
+
+  #
+  # LEDs (burner images use this for instance)
+  #
+  hardware.leds = [
+	  "beaglebone:black:heartbeat"
+	  "beaglebone:black:mmc0"
+	  "beaglebone:black:usr2"
+	  "beaglebone:black:usr3"
+  ];
+
+  #
+  # Burner Support
+  #
+  hardware.burner = {
+    disk = "/dev/mmcblk1";
+
+    # erase read only boot disk
+    preBurnScript = pkgs.writeScript "beaglebone-pre-burn" ''
+      #! ${pkgs.runtimeShell}
+      set -euxo pipefail
+
+      echo 0 > /sys/block/mmcblk1boot0/force_ro
+      echo 0 > /sys/block/mmcblk1boot1/force_ro
+
+      dd if=/dev/zero of=/dev/mmcblk1boot0 bs=512 count=4096
+      dd if=/dev/zero of=/dev/mmcblk1boot1 bs=512 count=4096
+    '';
+  };
+}
